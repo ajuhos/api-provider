dbf7ced814c4216df5d84b6dfdfd44d44ae10b80
Juhos Ádám <ajuhos@logiscool.com>
2019-04-21 21:32:45 +0200

    Fixes for resolvers, external edges and relations
    This commit adds LazyApiEdge which allows a service to start and init
    without connecting to the external service. Lazy edges can be created
    for external relations and these will only be resolved on the first
    use.

--- src/ApiProvider.ts
+++ src/ApiProvider.ts
@@ -1,13 +1,21 @@
-import {Api,ApiEdgeDefinition} from "api-core";
+import {Api, ApiResolver} from "api-core";
 import {ApiVersionProvider} from "./ApiVersionProvider";
 
 export class ApiProvider {
 
     private apis: Api[] = [];
+    private resolverFactory: ((api: Api) => ApiResolver)|undefined;
     private providers: ApiVersionProvider[] = [];
+
+    resolver(resolverFactory: (api: Api) => ApiResolver) {
+        this.resolverFactory = resolverFactory;
+        return this
+    }
 
     version(version: string): ApiVersionProvider {
         const api = new Api(version);
+        if(this.resolverFactory) api.resolver = this.resolverFactory(api);
+
         this.apis.push(api);
 
         const provider = new ApiVersionProvider(api);
